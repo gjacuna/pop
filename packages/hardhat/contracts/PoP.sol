e8a4bdf5f1051a380b0465c62b09ebecc0a0855f
Guillermo Acuña <gjacuna@gmail.com>
2023-04-01 00:49:49 -0700

    minor changes

--- packages/hardhat/contracts/PoP.sol
+++ packages/hardhat/contracts/PoP.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "base64-sol/base64.sol";
 
 contract PoP is ERC721Enumerable, Ownable {
@@ -43,6 +43,14 @@ contract PoP is ERC721Enumerable, Ownable {
         string memory ipfsHash = history.ipfsHash;
         
         return ipfsHash;
+    }
+
+    function tokenSignature(uint256 tokenId) public view returns (bytes memory) {
+        require(_exists(tokenId), "ERROR: Item does not exist");
+        ChatHistory memory history = histories[tokenId];
+        bytes memory signature = history.signature;
+        
+        return signature;
     }
 
     function withdrawFunds() external onlyOwner {
